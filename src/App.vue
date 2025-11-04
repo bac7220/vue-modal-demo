a940d17f554c29260a6f1fa4dc02b4da5c68da14
bac7220 <bacbac7220@gmail.com>
2025-11-04 19:25:35 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+import ModalWindow from './ModalWindow.vue'
+
+const isModalOpen = ref(false) 
+</script>
+
+<template>
+ <div>
+ <h1>モーダル練習アプリ</h1>
+ <button @click="isModalOpen = true">モーダルを開く</button>
+<transition name="scale">
+ <ModalWindow v-if="isModalOpen" @close="isModalOpen = false" />
+</transition>
+
+ </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.overlay{
+  position:fixed;
+  inset:0;
+  display:grid;
+  place-items:center;
+}
+
+.scale-enter-active,.scale-leave-active{
+  transform:scale(1);
+  opacity:1;
+transition:all 0.3s ease-in-out;
+}
+.scale-enter-to,.scale-leave-from{
+}
+.scale-enter-from,.scale-leave-to{
+  transform:scale(0.8);
+  opacity:0;
+transition :all 1s ease-in-out;
+}
+</style>
---
+++ src/ModalWindow.vue
@@ -0,0 +1,32 @@
+<script setup>
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+  <div class="wrapper" @click.self="emit('close')">
+    <div class="modal">
+    <p>モーダルウィンドウのコンポーネントです！</p>
+     <button @click="emit('close')">モーダルを閉じる</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper{
+  background-color: #333;
+  position:fixed;
+  top:0;
+  left:0;
+  width:100vw;
+  height:100vh;
+  display: grid;
+  place-items:center;
+  
+}
+.modal{
+  background-color: #aaa;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+</style>
